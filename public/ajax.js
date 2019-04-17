82020605686b69bbe672b30aa4a1ff51343b1680
Henry <henry@Henryde-iMac.local>
2019-04-17 17:37:05 +0800

    Write a post request when submitting the form

--- public/ajax.js
+++ public/ajax.js
@@ -1,3 +1,13 @@
 $.get('/todos', function(data) {
-    debugger
+    console.log(data);
 });
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    var formData = $(this).serialize();
+
+    $.post('/todos', formData, function(data) {
+        console.log(data);
+    });
+})
