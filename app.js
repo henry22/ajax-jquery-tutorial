9970e0a8b854675d6e6b3f42bed2ee57c9c2112b
Henry <henry@Henryde-iMac.local>
2019-04-18 11:12:51 +0800

    Change the delete request from express to ajax call, and set the debugger

--- app.js
+++ app.js
@@ -84,14 +84,17 @@ app.put("/todos/:id", function(req, res){
 });
 
 app.delete("/todos/:id", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
-      todo.remove();
-      res.redirect("/todos");
-   }
- }); 
+  Todo.findByIdAndRemove(req.params.id, function(err, todo){
+    if(err){
+      console.log(err);
+    } else {
+      if(req.xhr) {
+        res.json(todo);
+      } else {
+        res.redirect("/todos");
+      }
+    }
+  }); 
 });
 
 
--- public/ajax.js
+++ public/ajax.js
@@ -12,17 +12,31 @@
 //     });
 // });
 
+// $('form').submit(function (e) {
+//     e.preventDefault();
+
+//     var formData = $(this).serialize();
+//     var formAction = $(this).attr('action');
+
+//     $.ajax({
+//         url: formAction,
+//         data: formData,
+//         type: 'PUT',
+//         success: function(data) {
+//             console.log(data);
+//         }
+//     });
+// });
+
 $('form').submit(function (e) {
     e.preventDefault();
 
-    var formData = $(this).serialize();
     var formAction = $(this).attr('action');
 
     $.ajax({
         url: formAction,
-        data: formData,
-        type: 'PUT',
-        success: function(data) {
+        type: 'DELETE',
+        success: function (data) {
             debugger;
         }
     });
